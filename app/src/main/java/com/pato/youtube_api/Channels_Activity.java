cc47492ee5d5e0a965de2d2ee452beab284be017
Patiwagura <patiwagura@gmail.com>
2019-06-17 11:32:32 +0300

    Added Action listeners to menu buttons

--- app/src/main/java/com/pato/youtube_api/Channels_Activity.java
+++ app/src/main/java/com/pato/youtube_api/Channels_Activity.java
@@ -1,13 +1,18 @@
 package com.pato.youtube_api;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
 
 public class Channels_Activity extends AppCompatActivity implements View.OnClickListener {
+
+    Button btnAljezera, btnK2, btnK24;
+    private final String VID_URL = "VID_URL";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,12 +21,27 @@ public class Channels_Activity extends AppCompatActivity implements View.OnClick
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        //create references to the menu_buttons.
+        btnAljezera =(Button)findViewById(R.id.btnAljezera);
+        btnK2 = (Button) findViewById(R.id.btnCitizen);
+        btnK24 = (Button)findViewById(R.id.btnK24);
+
+        //set action listeners for the buttons.
+        btnAljezera.setOnClickListener(this);
+        btnK24.setOnClickListener(this);
+        btnK2.setOnClickListener(this);
+
+
         FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                Snackbar.make(view, "Going back to Main Activity", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
+
+                //go back to menu_activity.
+                Intent showMainAct = new Intent(Channels_Activity.this, MainActivity.class);
+                startActivity(showMainAct);
             }
         });
     }
@@ -32,17 +52,26 @@ public class Channels_Activity extends AppCompatActivity implements View.OnClick
 
             case R.id.btnAljezera:
                 //aljezeera clicked.
+                showVidActivity("jL8uDJJBjMA");
                 break;
 
             case R.id.btnK24:
-
+                showVidActivity("3g1ut_11xw0");
+                break;
+            case R.id.btnCitizen:
+                showVidActivity("xnUKJbUZoQM");
                 break;
 
         }
 
     }
 
-    //call method when button is clicked.
-    public void aljezeera_call(View view) {
+    //method called to show the Activity with youtubeApi.
+    public void showVidActivity(String mVidUrl){
+        Intent shoVidIntent = new Intent(Channels_Activity.this, MainActivity.class);
+        shoVidIntent.putExtra(VID_URL,mVidUrl);
+        startActivity(shoVidIntent);
+
     }
+
 }
--- app/src/main/java/com/pato/youtube_api/MainActivity.java
+++ app/src/main/java/com/pato/youtube_api/MainActivity.java
@@ -1,5 +1,6 @@
 package com.pato.youtube_api;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -16,28 +17,39 @@ public class MainActivity extends YouTubeBaseActivity {
     Button btnPlayVid ;
     YouTubePlayerView tubePlayerView;
     YouTubePlayer.OnInitializedListener onInitListener;
+    String myVidUrl = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //get video url from intent.
+        Intent mIntent = getIntent();
+        myVidUrl = mIntent.getStringExtra("VID_URL");
+
+
         //init the view widgets.
         tubePlayerView = (YouTubePlayerView) findViewById(R.id.youtube_view);
         btnPlayVid = (Button)findViewById(R.id.btnPlayVid);
-        btnPlayVid.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                tubePlayerView.initialize("AIzaSyBGGojsqpkgYdLx7w09VS5AuPZVvUdzdS0",onInitListener);
-            }
-        });
+//        btnPlayVid.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                tubePlayerView.initialize("AIzaSyBGGojsqpkgYdLx7w09VS5AuPZVvUdzdS0",onInitListener);
+//            }
+//        });
+
+
+
 
         //youtube initialization/
         onInitListener = new YouTubePlayer.OnInitializedListener() {
             @Override
             public void onInitializationSuccess(YouTubePlayer.Provider provider, YouTubePlayer youTubePlayer, boolean b) {
                 //load the video param: video url
-                youTubePlayer.loadVideo("9ZaEPeaucIU");
+                //youTubePlayer.loadVideo("9ZaEPeaucIU");
+                youTubePlayer.loadVideo(myVidUrl);
+
 
             }
 
@@ -46,5 +58,12 @@ public class MainActivity extends YouTubeBaseActivity {
 
             }
         };
+
+
+        //initialize the youtube player here.
+        tubePlayerView.initialize("AIzaSyBGGojsqpkgYdLx7w09VS5AuPZVvUdzdS0",onInitListener);
+    }
+
+    public void aljezeera_call(View view) {
     }
 }
